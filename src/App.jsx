5af0c7d7b523a6e112a37903af6382f80a0b69ba
mirzaakhmedov20 <mirzaakhmedovshukurullo20gmail.com>
2024-03-07 23:58:00 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import logo from "./assets/main.png";
+import logo from "./assets/IMAGE.png";
+import logo from "./assets/IMAGE-1.svg";
+import logo from "./assets/IMAGE-2.svg";
+import logo from "./assets/IMAGE-3.svg";
+import logo from "./assets/IMAGE-4.svg";
+import logo from "./assets/IMAGE-5.svg";
+import logo from "./assets/IMAGE-6.svg";
+import logo from "./assets/IMAGE-7.svg";
+function App() {
+  return (
+    <>
+      <img src="{logo}" alt="logo" /> 
+      <h1>Hello Shukurullo</h1>
+    </>
+  );
+}
+
+export default App;
